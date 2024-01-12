6419cf009c75b9dfb484b98640b9924e285e6597
Andy Yu <andyyu132@gmail.com>
2024-01-12 12:36:27 -0500

    Making my own folder

--- Vision/Andy.py
+++ Vision/Andy.py
@@ -43,36 +43,35 @@ y,x = np.where(mask)
 img = cv2.rectangle(img, (np.min(x), np.min(y)), (np.max(x), np.max(y)), (0,255,255), 3)
 ###
 
-# ### Alternatives to finding indexes of a value in array to consdier
-# # print(np.nonzero(y == max(y)))
-# # print(np.where(y == max(y)))
+### Alternatives to finding indexes of a value in array to consdier
+# print(np.nonzero(y == max(y)))
+# print(np.where(y == max(y)))
 
-# # Coordinate of left pixel
-# xLeft,yLeft = np.min(x),y[lastIndex(x,min(x))] # Maybe use variable for min(x) earlier
+# Coordinate of left pixel
+xLeft,yLeft = np.min(x),y[lastIndex(x,min(x))] # Maybe use variable for min(x) earlier
 
-# # Coordinate of right pixel
-# xRight,yRight = np.max(x),y[lastIndex(x,max(x))]
+# Coordinate of right pixel
+xRight,yRight = np.max(x),y[lastIndex(x,max(x))]
 
-# # Coordinate of top pixel
-# xTop, yTop = x[int((firstIndex(y,np.min(y))+lastIndex(y,np.min(y)))/2)],np.min(y)
+# Coordinate of top pixel
+xTop, yTop = x[int((firstIndex(y,np.min(y))+lastIndex(y,np.min(y)))/2)],np.min(y)
 
-# img[yLeft,xLeft] = (0,255,0)
-# img[yRight,xRight] = (0,255,0)
-# img[yTop,xTop] = (0,255,0)
+img[yLeft,xLeft] = (0,255,0)
+img[yRight,xRight] = (0,255,0)
+img[yTop,xTop] = (0,255,0)
 
-# cv2.line(img,(xLeft,yLeft),(xTop,yTop),(255,0,0),2)
-# cv2.line(img,(xRight,yRight),(xTop,yTop),(255,0,0),2)
+cv2.line(img,(xLeft,yLeft),(xTop,yTop),(255,0,0),2)
+cv2.line(img,(xRight,yRight),(xTop,yTop),(255,0,0),2)
 
-# angLeft = math.atan2(yLeft-yTop, xTop-xLeft)/2/math.pi*360
-# angRight = math.atan2(yRight-yTop, xRight-xTop)/2/math.pi*360
-# angDiff = 90-angLeft-angRight
+angLeft = math.atan2(yLeft-yTop, xTop-xLeft)/2/math.pi*360
+angRight = math.atan2(yRight-yTop, xRight-xTop)/2/math.pi*360
+angDiff = 90-angLeft-angRight
 
-# print(angLeft)
-# print(angRight)
-# print(30/angDiff)
+print(angLeft)
+print(angRight)
+print(30/angDiff)
 
 ###Drawing edges###
-
 
 edges = cv2.Canny(img,100,200)
 
