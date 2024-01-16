ff9e8f44dccbf7c01192c82b7f03bd0d0954380b
Anshul Gupta <hi.anshulg@gmail.com>
2024-01-16 16:28:00 -0500

    cv and img restructure

--- Vision/Anshul2.py
+++ Vision/Anshul2.py
@@ -1,40 +1,47 @@
 import cv2
 import numpy as np
 
-# Load the image
-image = cv2.imread('red.png')
+def process_frame(frame):
+    height, width = frame.shape[:2]
+    dimensions = (int(0.1 * width), int(0.1 * height))
+    scaled_frame = cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
 
-# Scale the image to 10%
-height, width = image.shape[:2]
-scaled_image = cv2.resize(image, (int(0.1 * width), int(0.1 * height)))
+    kernel = (7, 7)
+    sigmaX = 0
+    blurred = cv2.GaussianBlur(scaled_frame, kernel, sigmaX)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-scaled_image = cv2.GaussianBlur(scaled_image, (7, 7), 0)
+    lower_red1 = np.array([0, 120, 70])
+    upper_red1 = np.array([10, 255, 255])
+    lower_red2 = np.array([170, 120, 70])
+    upper_red2 = np.array([180, 255, 255])
 
-# Convert the scaled image to the HSV color space
-hsv = cv2.cvtColor(scaled_image, cv2.COLOR_BGR2HSV)
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    cmask = cv2.bitwise_or(mask1, mask2)
+    contours = cv2.findContours(cmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-# Define a lower and upper threshold for red color in HSV
-lower_red1 = np.array([0, 100, 20])
-upper_red1 = np.array([20, 255, 255])
+    if contours:
+        largest_contour = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        origin = (x, y)
+        endpoint = (x + w, y + h)
+        cv2.rectangle(scaled_frame, origin, endpoint, (255, 0, 0), 2)
 
-lower_red2 = np.array([159, 100, 20])
-upper_red2 = np.array([179, 255, 255])
+    return scaled_frame
 
-# Create a binary mask for red regions
-mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
-mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+video_input = cv2.VideoCapture('img/red.MOV') 
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+output = cv2.VideoWriter('tests/video/output_red.MOV', fourcc, 20.0, (640, 480)) 
 
-cmask = cv2.bitwise_or(mask1, mask2)
+while True:
+    ret, frame = video_input.read()
+    if not ret:
+        break
 
+    processed_frame = process_frame(frame)
+    output.write(processed_frame)
 
-contours, _ = cv2.findContours(cmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-for cnt in contours:
-    x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(scaled_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-
-# Display the original image, scaled image, and the result
-cv2.imshow('Red Object Detection', scaled_image)
-cv2.waitKey(0)
+video_input.release()
+output.release()
 cv2.destroyAllWindows()
