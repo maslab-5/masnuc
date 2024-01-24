7c18b5d61bf3c1d24042caa660d281b83ccf75b1
Anshul Gupta <hi.anshulg@gmail.com>
2024-01-24 00:34:45 -0500

    pathfinding queue with blocked off coords

--- Path/path_queue.py
+++ Path/path_queue.py
@@ -1,8 +1,11 @@
-def path(n, blocks, start):
+def path(n, blocks, start, blocked_coords):
     grid = [[0] * n for _ in range(n)]
     for block in blocks:
         grid[block[0]][block[1]] = 1
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    for blocked in blocked_coords:
+        grid[blocked[0]][blocked[1]] = 2
+
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
     queue = [((start[0], start[1]), [(start[0], start[1])], set(blocks))]
     visited = set([(start[0], start[1], tuple(blocks))])
 
@@ -13,7 +16,12 @@ def path(n, blocks, start):
         for dx, dy in directions:
             nx, ny = x + dx, y + dy
             next = (nx, ny, tuple(sorted(remaining_blocks)))
-            if 0 <= nx < n and 0 <= ny < n and next not in visited:
+            if (
+                0 <= nx < n
+                and 0 <= ny < n
+                and grid[nx][ny] != 2
+                and next not in visited
+            ):
                 if (nx, ny) in remaining_blocks:
                     new_remaining_blocks = remaining_blocks - {(nx, ny)}
                 else:
@@ -25,6 +33,7 @@ def path(n, blocks, start):
 
 n = 5
 blocks = [(1, 2), (2, 2), (3, 2), (2, 3), (2, 1)]
+blocked = [(0, 2), (4, 2)]
 start = (0, 0)
-result = path(n, blocks, start)
+result = path(n, blocks, start, blocked)
 print(result)
